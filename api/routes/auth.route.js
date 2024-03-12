d540c07135ee0971ea6831532211ee2fac005c28
Kevin Hu <hellokevinhu@icloud.com>
2024-03-12 17:12:45 +0800

    add functionality to the sign up page and fix routes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,6 +3,6 @@ const { signup } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/sign-up', signup);
 
 module.exports = router;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,48 @@
-import { Button, Label, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessages, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch('/api/auth/sign-up', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+
+      if (res.ok) {
+        console.log(res);
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-5 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -25,28 +66,51 @@ export default function SignUp() {
 
         {/* right */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label className='text-lg' value='Your username' />
-              <TextInput type='text' placeholder='Username' id='username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={handleChange}
+              />
             </div>
 
             <div>
               <Label className='text-lg' value='Your email' />
-              <TextInput type='text' placeholder='Email' id='email' />
+              <TextInput
+                type='text'
+                placeholder='Email'
+                id='email'
+                onChange={handleChange}
+              />
             </div>
 
             <div>
               <Label className='text-lg' value='Your password' />
-              <TextInput type='password' placeholder='Password' id='password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+              />
             </div>
 
             <Button
               className='font-semibold'
               gradientDuoTone='purpleToPink'
               type='submit'
+              disabled={loading}
             >
-              Sign Up
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
 
@@ -56,6 +120,12 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+
+          {errorMessages && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessages}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
