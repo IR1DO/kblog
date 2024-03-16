0f8d2a8b0a261f896f5edcafc274cd5e357c3387
Kevin Hu <hellokevinhu@icloud.com>
2024-03-16 21:40:41 +0800

    update header component with user data

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+        source: ['"Source Sans 3"'],
+      },
+    },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require('flowbite/plugin')],
 };
