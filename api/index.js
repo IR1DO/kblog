a21ab1690bec35383c32940b2af5272f23ebae18
Kevin Hu <hellokevinhu@icloud.com>
2024-03-18 21:22:13 +0800

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user.route');
 const authRoutes = require('./routes/auth.route');
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
