cf23c56b6fd5d8994327b8738f5d12def08b3951
Kevin Hu <hellokevinhu@icloud.com>
2024-03-25 21:20:23 +0800

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,7 @@
 import { Select, TextInput, FileInput, Button, Alert } from 'flowbite-react';
-import DashSidebar from '../components/DashSidebar';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   getDownloadURL,
   getStorage,
@@ -12,6 +11,7 @@ import {
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 import ImageCropDialog from '../components/ImageCropDialog';
 
 export default function CreatePost() {
@@ -20,6 +20,9 @@ export default function CreatePost() {
   const [imageUploadError, setImageUploadError] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
 
   const [crop, setCrop] = useState(false);
   const [imageStyle, setImageStyle] = useState({});
@@ -70,6 +73,31 @@ export default function CreatePost() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      } else {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+    }
+  };
+
   const handleCropComplete = (croppedArea) => {
     const SCALE = { x: 100 / croppedArea.width, y: 100 / croppedArea.height };
 
@@ -84,112 +112,120 @@ export default function CreatePost() {
     setCrop(false);
   };
 
+  useEffect(() => {
+    setFormData((prevData) => ({ ...prevData, imageStyle: imageStyle }));
+  }, [imageStyle]);
+
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className='md:w-56'>
-        {/* Sidebar */}
-        <DashSidebar />
-      </div>
+    <div className='p-3 w-9/12 mx-auto min-h-screen '>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
 
-      {/* profile... */}
-      <div className='p-3 w-9/12 mx-auto min-h-screen '>
-        <h1 className='text-center text-3xl my-7 font-semibold'>
-          Create a post
-        </h1>
-
-        <form className='flex flex-col gap-4'>
-          <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-            <TextInput
-              id='title'
-              type='text'
-              placeholder='Title'
-              className='[&_input]:text-base flex-1'
-              required
-            />
-
-            <Select className='[&_select]:text-base'>
-              <option value='uncategorized'>Select a category</option>
-              <option value='technology'>Technology</option>
-              <option value='programming'>Programming</option>
-              <option value='artificial-intelligence'>
-                Artificial Intelligence
-              </option>
-              <option value='web-development'>Web Development</option>
-              <option value='mobile-development'>Mobile Development</option>
-              <option value='data-science'>Data Science</option>
-              <option value='cybersecurity'>Cybersecurity</option>
-              <option value='cloud-computing'>Cloud Computing</option>
-              <option value='internet-of-things'>
-                Internet of Things (IoT)
-              </option>
-              <option value='robotics'>Robotics</option>
-              <option value='virtual-reality'>Virtual Reality (VR)</option>
-            </Select>
-          </div>
-
-          <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
-            <FileInput
-              type='file'
-              accept='image/*'
-              onChange={(e) => setFile(e.target.files[0])}
-            />
-            <Button
-              type='button'
-              gradientDuoTone='purpleToBlue'
-              size='sm'
-              onClick={handleUploadImage}
-              disabled={!file || imageFileUploading}
-            >
-              {imageFileUploading ? (
-                <div className='w-6 h-6'>
-                  <CircularProgressbar value={imageUploadProgress || 0} />
-                </div>
-              ) : (
-                'Upload Image'
-              )}
-            </Button>
-          </div>
-
-          {imageUploadError && (
-            <Alert color='failure'>{imageUploadError}</Alert>
-          )}
-
-          {formData.image && !crop && (
-            <div className='w-full aspect-[5/2] relative overflow-hidden'>
-              <img
-                src={formData.image}
-                alt='upload'
-                className='w-full absolute origin-top-left'
-                style={imageStyle}
-                onClick={() => {
-                  setCrop(true);
-                }}
-              />
-            </div>
-          )}
-
-          {crop && (
-            <ImageCropDialog
-              image={formData.image}
-              onComplete={handleCropComplete}
-            />
-          )}
-
-          <ReactQuill
-            theme='snow'
-            placeholder='Write something...'
-            className='h-72 mb-12 [&_.ql-editor]:font-mono [&_.ql-editor]:text-lg [&_.ql-editor::before]:dark:text-[rgba(255,255,255,0.6)] [&_.ql-picker]:dark:text-white [&_.ql-picker-options]:dark:bg-slate-400 [&_.ql-toolbar]:dark:bg-slate-400 [&_h1]:mb-2 [&_h1]:mt-12 [&_h2]:mb-2 [&_h2]:mt-9 [&_h3]:mb-2 [&_h3]:mt-6'
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-4 sm:flex-row justify-between'>
+          <TextInput
+            id='title'
+            type='text'
+            placeholder='Title'
+            className='[&_input]:text-base flex-1'
             required
+            onChange={(e) => {
+              setFormData({ ...formData, title: e.target.value });
+            }}
+          />
+
+          <Select
+            className='[&_select]:text-base'
+            onChange={(e) => {
+              setFormData({ ...formData, category: e.target.value });
+            }}
+          >
+            <option value='uncategorized'>Select a category</option>
+            <option value='technology'>Technology</option>
+            <option value='programming'>Programming</option>
+            <option value='artificial-intelligence'>
+              Artificial Intelligence
+            </option>
+            <option value='web-development'>Web Development</option>
+            <option value='mobile-development'>Mobile Development</option>
+            <option value='data-science'>Data Science</option>
+            <option value='cybersecurity'>Cybersecurity</option>
+            <option value='cloud-computing'>Cloud Computing</option>
+            <option value='internet-of-things'>Internet of Things (IoT)</option>
+            <option value='robotics'>Robotics</option>
+            <option value='virtual-reality'>Virtual Reality (VR)</option>
+          </Select>
+        </div>
+
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+          <FileInput
+            type='file'
+            accept='image/*'
+            onChange={(e) => setFile(e.target.files[0])}
           />
           <Button
-            type='submit'
-            gradientDuoTone='purpleToPink'
-            className='font-bold [&>span]:text-base'
+            type='button'
+            gradientDuoTone='purpleToBlue'
+            size='sm'
+            onClick={handleUploadImage}
+            disabled={!file || imageFileUploading}
           >
-            Publish
+            {imageFileUploading ? (
+              <div className='w-6 h-6'>
+                <CircularProgressbar value={imageUploadProgress || 0} />
+              </div>
+            ) : (
+              'Upload Image'
+            )}
           </Button>
-        </form>
-      </div>
+        </div>
+
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+
+        {formData.image && !crop && (
+          <div className='w-full aspect-[5/2] relative overflow-hidden'>
+            <img
+              src={formData.image}
+              alt='upload'
+              className='w-full absolute origin-top-left'
+              style={imageStyle}
+              onClick={() => {
+                setCrop(true);
+              }}
+            />
+          </div>
+        )}
+
+        {crop && (
+          <ImageCropDialog
+            image={formData.image}
+            onComplete={handleCropComplete}
+          />
+        )}
+
+        <ReactQuill
+          theme='snow'
+          placeholder='Write something...'
+          className='h-72 mb-12 [&_.ql-editor]:font-mono [&_.ql-editor]:text-lg [&_.ql-editor::before]:dark:text-[rgba(255,255,255,0.6)] [&_.ql-picker]:dark:text-white [&_.ql-picker-options]:dark:bg-slate-400 [&_.ql-toolbar]:dark:bg-slate-400 [&_h1]:pb-4 [&_h1]:pt-12 [&_h2]:pb-2 [&_h2]:pt-9 [&_h3]:pb-2 [&_h3]:pt-6'
+          required
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+          }}
+        />
+
+        <Button
+          type='submit'
+          gradientDuoTone='purpleToPink'
+          className='font-bold [&>span]:text-base'
+        >
+          Publish
+        </Button>
+
+        {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
+      </form>
     </div>
   );
 }
