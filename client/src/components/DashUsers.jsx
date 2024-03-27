93cfca387f02b61b6f69779f370ea2fea8ef845d
Kevin Hu <hellokevinhu@icloud.com>
2024-03-27 21:00:57 +0800

    add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -53,10 +53,9 @@ export default function DashUsers() {
     setShowModal(false);
 
     try {
-      const res = await fetch(
-        `/api/user/deleteuser/${userIdToDelete}/${currentUser._id}`,
-        { method: 'DELETE' }
-      );
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: 'DELETE',
+      });
 
       const data = await res.json();
       if (!res.ok) {
