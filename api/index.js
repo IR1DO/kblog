b7cf13b23a88e30384994ad83cf85a0d4616fa45
Kevin Hu <hellokevinhu@icloud.com>
2024-04-04 16:17:13 +0800

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/auth.route');
 const postRoutes = require('./routes/post.route');
 const commentRoutes = require('./routes/comment.route');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +34,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
