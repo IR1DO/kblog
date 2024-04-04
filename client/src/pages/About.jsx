445fae80c3e5f6f86a90ac3f0cf12e4a9888faef
Kevin Hu <hellokevinhu@icloud.com>
2024-04-04 15:55:50 +0800

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,34 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='mx-auto p-8 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>
+            About KBlog
+          </h1>
+          <div className='text-base text-gray-500 flex flex-col gap-4'>
+            <p>
+              At KBlog, we are dedicated to providing you with the latest, most
+              intriguing content in technology, science, culture, and lifestyle.
+              Our mission is to present depth, insight, and innovation to our
+              readers while maintaining diversity and freshness in our content.
+            </p>
+            <p>
+              Our team consists of experienced writers, editors, and experts
+              committed to delivering high-quality articles, tutorials, and
+              exclusive reports. Whether you're interested in cutting-edge
+              technology, scientific discoveries, cultural trends, or lifestyle
+              tips, KBlog has something for you.
+            </p>
+            <p>
+              We strive for true insight and understanding at KBlog, and we
+              encourage our readers to explore, learn, and engage with the
+              diverse topics we cover. Join us on our journey to discover the
+              world's most fascinating stories and ideas.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,3 +1,13 @@
+import CallToAction from '../components/CallToAction';
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className='min-h-screen max-w-2xl mx-auto flex flex-col gap-4 items-center justify-center p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-base text-gray-500'>
+        Build fun and engaging projects while learning JavaScript
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
